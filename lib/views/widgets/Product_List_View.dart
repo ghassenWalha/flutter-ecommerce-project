00d7644019bc3b3f905e1ff2a91e98c68cb85097
souheilbenslama <benslamasouheil@gmail.com>
2020-11-18 21:01:31 +0100

    adding .map

--- lib/views/widgets/Product_List_View.dart
+++ lib/views/widgets/Product_List_View.dart
@@ -4,23 +4,23 @@ import 'package:flutter_ecommerce_project/views/widgets/Product_View.dart';
 
 class ProductListView extends StatelessWidget {
   final List<Product> productList;
-  final List<ProductView> productListView = [];
-  ProductListView({this.productList}) {
-    productList.forEach((element) {
-      productListView.add(ProductView(product: element));
-    });
-  }
+
+  /// final List<ProductView> productListView = [];
+
+  ProductListView(this.productList);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: GridView.count(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: GridView.count(
           crossAxisCount: 2,
           mainAxisSpacing: 7,
           crossAxisSpacing: 7,
-          children: productListView),
-    );
+          children: [
+            ...productList.map((produit) => ProductView(produit)).toList()
+          ],
+        ));
   }
 }
--- lib/views/widgets/Product_View.dart
+++ lib/views/widgets/Product_View.dart
@@ -3,9 +3,9 @@ import '../../models/product.dart';
 
 class ProductView extends StatefulWidget {
   final Product product;
-  ProductView({
+  ProductView(
     this.product,
-  });
+  );
   @override
   _ProductViewState createState() => _ProductViewState();
 }
@@ -14,73 +14,63 @@ class _ProductViewState extends State<ProductView> {
   bool isFav = false;
   @override
   Widget build(BuildContext context) {
-    return Hero(
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.4,
+      child: Hero(
         tag: this.widget.product.id,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: InkWell(
-                enableFeedback: true,
-                onDoubleTap: () {
-                  setState(() {
-                    isFav = !isFav;
-                  });
-                },
-                child: Container(
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: IconButton(
-                      icon: isFav
-                          ? Icon(
-                              Icons.favorite,
-                              color: Colors.red,
-                            )
-                          : Icon(
-                              Icons.favorite_outline,
-                              color: Colors.black,
-                            ),
-                      onPressed: () {
-                        setState(() {
-                          isFav = !isFav;
-                        });
-                      },
-                    ),
-                  ),
-                  width: MediaQuery.of(context).size.width / 2.5,
-                  height: MediaQuery.of(context).size.width / 3,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(18),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        this.widget.product.imgUrl,
-                      ),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-                width: MediaQuery.of(context).size.width / 2.5,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            child: InkWell(
+              enableFeedback: true,
+              onDoubleTap: () {
+                setState(() {
+                  isFav = !isFav;
+                });
+              },
+              child: Container(
                 child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    this.widget.product.name,
-                    style: TextStyle(fontSize: 25),
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    icon: isFav
+                        ? Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          )
+                        : Icon(
+                            Icons.favorite_outline,
+                            color: Colors.black,
+                          ),
+                    onPressed: () {
+                      setState(() {
+                        isFav = !isFav;
+                      });
+                    },
                   ),
-                )),
-            Container(
-              width: MediaQuery.of(context).size.width / 2.5,
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "\$${this.widget.product.price} ",
-                  style: TextStyle(color: Colors.grey, fontSize: 15),
+                ),
+                width: MediaQuery.of(context).size.width / 3.5,
+                height: MediaQuery.of(context).size.width / 4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      this.widget.product.imgUrl,
+                    ),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+          Text(
+            this.widget.product.name,
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            "\$${this.widget.product.price} ",
+            style: TextStyle(color: Colors.grey, fontSize: 15),
+          ),
+        ]),
+      ),
+    );
   }
 }
