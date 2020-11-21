fe5a2b8859b2c5f5ed96f4dd0cc8984e3f89dbc8
missaouiamal <amal.missaoui@insat.u-carthage.tn>
2020-11-21 20:37:51 +0100

    nahina l my_home_page

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ProductPage(),
     );
   }
 }
