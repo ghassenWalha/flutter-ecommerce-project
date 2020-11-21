533f30690a5c18f06f93fccaae33a7c2630b9356
souheilbenslama <benslamasouheil@gmail.com>
2020-11-21 20:42:11 +0100

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './views/screens/my_home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -14,8 +15,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
