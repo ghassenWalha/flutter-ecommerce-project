f780fdfd89623bfd81eaf1f69a6a763d2e203a69
ghassenWalha <walha.ghassen@insat.u-carthage.tn>
2020-11-22 08:31:55 +0100

    error fixing after merging

--- lib/fixtures/fixture.dart
+++ lib/fixtures/fixture.dart
@@ -1,28 +1,28 @@
 /*This is the fake data */
 List<Map<String, dynamic>> bagList = [
   {
-    'imageUrl': 'assets/velvet_chair.jpg',
+    'imageUrl': 'velvet_chair.jpg',
     'price': 300.00,
     'color': 'pink',
     'name': 'Velvet Chair',
     'quantity': 1,
   },
   {
-    'imageUrl': 'assets/sofa.jpg',
+    'imageUrl': 'sofa.jpg',
     'price': 350.00,
     'color': 'yellow',
     'name': 'sofa',
     'quantity': 1,
   },
   {
-    'imageUrl': 'assets/chair.jpg',
+    'imageUrl': 'chair.jpg',
     'price': 200.00,
     'color': 'Grey',
     'name': 'Chair',
     'quantity': 1,
   },
   {
-    'imageUrl': 'assets/table.jpg',
+    'imageUrl': 'table.jpg',
     'price': 150.00,
     'color': 'Grey',
     'name': 'Table',
