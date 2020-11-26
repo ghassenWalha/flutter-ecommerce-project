3a08cc3eeb2f2517ae4b2591aee8157b9a84ed70
missaouiamal <amal.missaoui@insat.u-carthage.tn>
2020-11-26 19:14:18 +0100

    First push

--- lib/views/screens/edit_product.dart
+++ lib/views/screens/edit_product.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter_ecommerce_project/models/product.dart';
+
+import '../widgets/add_image.dart';
+import '../widgets/add_details.dart';
+import '../widgets/customized_button.dart';
 
 class EditProduct extends StatefulWidget {
   @override
@@ -8,6 +13,31 @@ class EditProduct extends StatefulWidget {
 class _State extends State<EditProduct> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //final Product pp = ModalRoute.of(context).settings.arguments;
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            AddImage(),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 24.0,
+                left: 24.0,
+                right: 24.0,
+                bottom: 4.0,
+              ),
+              child: AddDetails(),
+            ),
+          ],
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CustomizedButton('Cancel'),
+            CustomizedButton('Save'),
+          ],
+        ),
+      ),
+    );
   }
 }
