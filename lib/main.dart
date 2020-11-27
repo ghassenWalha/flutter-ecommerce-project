238a756d48415d15187a4aa0b175adfc4a6534f7
ghassenWalha <67965351+ghassenWalha@users.noreply.github.com>
2020-11-27 10:05:36 +0100

    Revert "Sprint2/edit product"

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_project/models/product.dart';
-import 'package:flutter_ecommerce_project/views/widgets/category_filter.dart';
-
 import './views/screens/my_home_page.dart';
-
-import './views/screens/product_details_screen.dart';
-
-import './views/screens/bag_screen.dart';
-
-import 'package:flutter_ecommerce_project/views/screens/admin_screen.dart';
-import './views/screens/edit_product.dart';
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', initialRoute: '/', routes: {
-      // When navigating to the "/" route, build the FirstScreen widget.
-      '/': (context) => MyHomePage(),
-      // When navigating to the "/second" route, build the SecondScreen widget.
-      '/product_details_screen': (context) => ProductPage(),
-      '/category_filter': (context) => CategoryFilter()
-    });
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
+
