534b1d64ebe72aa07ca7388b6c1167f96d9ee6b4
souheilbenslama <benslamasouheil@gmail.com>
2020-12-19 17:28:10 +0100

    fixing my home page

--- lib/views/screens/my_home_page.dart
+++ lib/views/screens/my_home_page.dart
@@ -15,7 +15,6 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -24,10 +23,9 @@ class _MyHomePageState extends State<MyHomePage> {
   PageController pageController;
   int currentIndex = 0;
   Map decodedToken;
-
+  String token;
   void onSelect(int index) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       this.currentIndex = index;
       pageController.animateToPage(
@@ -58,51 +56,84 @@ class _MyHomePageState extends State<MyHomePage> {
   ];
   @override
   Widget build(BuildContext context) {
-    String token;
-    return Container(
-        child: FutureBuilder(
-            future: SharedPreferences.getInstance(),
-            builder: (BuildContext context,
-                AsyncSnapshot<SharedPreferences> snapshot) {
-              if (snapshot.hasData) {
-                token = snapshot.data.getString("token");
-                if (token != null) {
-                  decodedToken = JwtDecoder.decode(token);
-                  if (decodedToken["isAdmin"]) {
-                    // change the navbar if the usr is admin
-                    _screens[1] = Center(child: BagScreen());
-                  }
-                }
-              } else {}
-              return Scaffold(
-                  body: PageView(
-                    controller: pageController,
-                    onPageChanged: (index) async {
-                      setState(() {
-                        currentIndex = index;
-                      });
-                    },
-                    children: _screens,
-                  ),
-                  backgroundColor: Colors.white,
-                  bottomNavigationBar: BottomNavigationBar(
-                    type: BottomNavigationBarType.fixed,
-                    items: [
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.home_outlined), label: "Home"),
-                      ((token != null) && decodedToken["isAdmin"])
-                          ? BottomNavigationBarItem(
-                              icon: Icon(Icons.lock), label: "admin Dashbord")
-                          : BottomNavigationBarItem(
-                              icon: Icon(Icons.favorite), label: "favorite"),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.shopping_bag), label: "Bag"),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.person), label: "login/sign in"),
-                    ],
-                    currentIndex: currentIndex >= 3 ? 3 : currentIndex,
-                    onTap: onSelect,
-                  ));
-            }));
+    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+    return FutureBuilder(
+        future: prefs,
+        builder:
+            (BuildContext context, AsyncSnapshot<SharedPreferences> snapshot) {
+          if (snapshot.hasData) {
+            token = snapshot.data.getString("token");
+            if (token != null) {
+              decodedToken = JwtDecoder.decode(token);
+              if (decodedToken["isAdmin"]) {
+                // change the navbar if the usr is admin
+                _screens[1] = Center(child: AdminScreen());
+              }
+            }
+          } else {
+            // return Container(child: Text("hello world"));
+          }
+          return Scaffold(
+              body: PageView(
+                controller: pageController,
+                onPageChanged: (index) async {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                children: _screens,
+              ),
+              backgroundColor: Colors.white,
+              bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                items: [
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home_outlined), label: "Home"),
+                  ((token != null) && decodedToken["isAdmin"])
+                      ? BottomNavigationBarItem(
+                          icon: Icon(Icons.lock), label: "admin Dashbord")
+                      : BottomNavigationBarItem(
+                          icon: Icon(Icons.favorite), label: "favorite"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.shopping_bag), label: "Bag"),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.person), label: "login/sign in"),
+                ],
+                currentIndex: currentIndex >= 3 ? 3 : currentIndex,
+                onTap: onSelect,
+              ));
+        });
   }
 }
+
+/* return Scaffold(
+        body: PageView(
+          controller: pageController,
+          onPageChanged: (index) async {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          children: _screens,
+        ),
+        backgroundColor: Colors.white,
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: "Home"),
+            // ((token != null) && decodedToken["isAdmin"])
+            // BottomNavigationBarItem(
+            //  icon: Icon(Icons.lock), label: "admin Dashbord")
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite), label: "favorite"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag), label: "Bag"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person), label: "login/sign in"),
+          ],
+          currentIndex: currentIndex >= 3 ? 3 : currentIndex,
+          onTap: onSelect,
+        ));
+  }
+} */
