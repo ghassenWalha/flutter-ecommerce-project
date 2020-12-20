bd1e52d840aca72a4255c3daee6aca3bb0674901
zohrakhay <zohrakhay999@gmail.com>
2020-12-20 16:53:49 +0100

    Fixing BagProblem

--- lib/views/widgets/bag_list.dart
+++ lib/views/widgets/bag_list.dart
@@ -17,8 +17,13 @@ class BagList extends StatefulWidget {
 
 class BagListState extends State<BagList> {
   final bagService = BagService();
-
-  List bagList;
+  bool isFirstTime = true;
+  List<Map<String, dynamic>> bagList;
+  Future<List<Product>> futurebagList;
+  @override
+  void initState() {
+    futurebagList = bagService.getBag();
+  }
 
   /*This function removes a widget when we click on the  delete button*/
   void remove(id) {
@@ -27,12 +32,12 @@ class BagListState extends State<BagList> {
     });
   }
 
-/*This function adds 1 to the quantity when we click on the plus button
+//This function adds 1 to the quantity when we click on the plus button
   void addQuantity(int id) {
     setState(() {
-      list[id]['quantity'] = list[id]['quantity'] + 1;
+      bagList[id]['quantity'] = bagList[id]['quantity'] + 1;
     });
-  }*/
+  }
 
 /*This function substract 1 from the quantity when we click on the minus button*/
 //  void substractQuantity(int id) {}
@@ -46,31 +51,37 @@ class BagListState extends State<BagList> {
         SizedBox(
             height: MediaQuery.of(context).size.height * 0.725,
             child: FutureBuilder(
-                future: bagService.getBag(),
+                future: futurebagList,
                 builder: (BuildContext context,
                     AsyncSnapshot<List<Product>> snapshot) {
                   if (snapshot.hasData) {
                     List<Product> products = snapshot.data;
 
                     print(products);
+                    int q = 1;
+                    if (isFirstTime) {
+                      bagList = products
+                          .map((produit) => {'product': produit, 'quantity': q})
+                          .toList();
 
-                    /*List*/ bagList = products
-                        .map((produit) => {"product": produit, "quantity": 1})
-                        .toList();
+                      isFirstTime = false;
+                    }
 
                     return ListView.builder(
-                        itemCount: products.length,
+                        itemCount: bagList.length,
                         itemBuilder: (context, i) {
+                          // print(bagList[i]);
                           return BagItem(
-                            product: bagList[i]["product"],
-                            quantity: bagList[i]["quantity"],
-                            //  key: ValueKey(bagList[i]["product"]["name"]),
+                            product: bagList[i]['product'],
+                            quantity: bagList[i]['quantity'],
+                            key: ValueKey(bagList[i]['product'].name),
                             remove: remove,
                             addQuantity: () {
                               setState(() {
-                                bagList[i]['quantity'] =
-                                    bagList[i]['quantity'] + 1;
+                                bagList[i]["quantity"] =
+                                    bagList[i]["quantity"] + 1;
                               });
+                              // addQuantity(i);
                             },
                             substractQuantity: () {
                               setState(() {
@@ -90,8 +101,8 @@ class BagListState extends State<BagList> {
                 })),
         SizedBox(
             height: MediaQuery.of(context).size.height * 0.06,
-            child: Total(
-                bagList)) //This is the widget responsible for displaying the total
+            child: Total(bagList ??
+                [])) //This is the widget responsible for displaying the total
       ]),
     );
   }
